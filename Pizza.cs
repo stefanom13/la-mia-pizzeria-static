6379cb60dd00c7018b26e1c99264d31917473e44
stefanom13 <stefanomancuso13@gmail.com>
2022-07-13 15:42:38 +0200

    esercizio completato. Sistemare grafica

---
+++ Pizza.cs
@@ -0,0 +1,20 @@
+﻿namespace la_mia_pizzeria_static
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string NomePizza { get; set; }
+        public string Descrizione { get; set; }
+        public string PathImage { get; set; }
+        public float Prezzo { get; set; }
+
+        public Pizza(int id, string nomePizza, string descrizione, string pathImage, float prezzo)
+        {
+            Id = id;
+            NomePizza = nomePizza;
+            Descrizione = descrizione;
+            PathImage = pathImage;
+            Prezzo = prezzo;
+        }
+    }
+}
